20fe366d3525671416943b3ddf1868051078c6b2
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-08-21 10:35:49 +0530

    created form to take input for new event in the list

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,17 +1,40 @@
+import { useState } from 'react'
 import './NewEventForm.css'
 
 export default function NewEventForm() {
+
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState('')
+
+    const resetForm = () => {
+        setTitle('')
+        setDate('')
+    }
+
+    const handleSubmit = ()=> {
+        
+    }
     return (
-        <form className="new-event-form">
+        <form className="new-event-form" onSubmit={handleSubmit}>
             <label>
                 <span>Event Title: </span>
-                <input type="text" />
+                <input
+                    type="text"
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
             </label>
             <label>
                 <span>Event Date: </span>
-                <input type="date" />
+                <input
+                    type="date"
+                    onChange={(e) => setDate(e.target.value)}
+                    value={date}
+                />
             </label>
             <button >Submit</button>
+            <p>title - {title}, date - {date}</p>
+            <p onClick={resetForm} >reset the form</p>
         </form>
     )
 }
