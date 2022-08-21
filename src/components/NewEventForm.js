285b601074967e37a5e1a01991d541769b8a7fcc
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-08-21 15:44:41 +0530

    REplaced useState method of states with useRef method of references

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,22 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './NewEventForm.css'
 
-export default function NewEventForm({addEvent}) {
+export default function NewEventForm({ addEvent }) {
 
-    const [title, setTitle] = useState('')
-    const [date, setDate] = useState('')
+    // const [title, setTitle] = useState('')
+    // const [date, setDate] = useState('')
+
+    const title = useRef()
+    const date = useRef()
 
     const resetForm = () => {
-        setTitle('')
-        setDate('')
+        title.current.value = ""
+        date.current.value = ""
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-
+        console.log(title, date)
         const event = {
-            title: title,
-            date: date,
+            title: title.current.value,
+            date: date.current.value,
             id: Math.floor(Math.random() * 10000)
         }
         addEvent(event)
@@ -28,21 +31,19 @@ export default function NewEventForm({addEvent}) {
                 <span>Event Title: </span>
                 <input
                     type="text"
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
+                    ref = {title}
                 />
             </label>
             <label>
                 <span>Event Date: </span>
                 <input
                     type="date"
-                    onChange={(e) => setDate(e.target.value)}
-                    value={date}
+                    ref= {date}
                 />
             </label>
             <button >Submit</button>
-            <p>title - {title}, date - {date}</p>
-            <p onClick={resetForm} >reset the form</p>
+            {/* <p>title - {title}, date - {date}</p>
+            <p onClick={resetForm} >reset the form</p> */}
         </form>
     )
 }
